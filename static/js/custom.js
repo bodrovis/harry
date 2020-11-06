8aad59a2858b1561a4536ae36a4b2ea744c554b8
bodrovis <bodrovis@protonmail.com>
2020-11-06 22:57:40 +0200

    bump

---
+++ static/js/custom.js
@@ -0,0 +1,24 @@
+function update_hash() {
+  document.location.hash = '#' + this.attr('id');
+}
+
+$(document).ready(function() {
+  $('.book-main .page-num').click(function(e) {
+    update_hash.apply($(this).closest('.row'));
+  });
+
+  $('.chapter-title').click(function(e) {
+    update_hash.apply($(this));
+  });
+
+  let $index = $('.index-wrapper');
+  if($index.length > 0) {
+    let index_contents = '<h5>Оглавление этой части</h5><ul class="index">';
+    $('.chapter-title').each(function() {
+      let $this = $(this);
+      index_contents += '<li><a href="#' + $this.attr('id') + '">' + $this.text() + '</a></li>';
+    });
+    index_contents += '</ul>';
+    $index.html(index_contents);
+  }
+});
