89274796fd26d3c68bc56363b337f68f8580c52c
bodrovis <bodrovis@protonmail.com>
2020-11-17 06:23:31 +0200

    bump

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,6 +3,27 @@ function update_hash() {
 }
 
 $(document).ready(function() {
+  age_confirmed = Cookies.get('harry-age-confirmed');
+  if (!age_confirmed) {
+    $main = $('.main-content');
+    $age_check = $('.age-check');
+
+    $main.hide();
+    $age_check.show();
+
+    $('.js-age-yes').click(function(e) {
+      e.preventDefault();
+      Cookies.set('harry-age-confirmed', 'true', { sameSite: 'strict',  expires: 7 });
+      $main.show();
+      $age_check.hide();
+    });
+
+    $('.js-age-no').click(function(e) {
+      e.preventDefault();
+      $age_check.hide();
+    });
+  }
+
   $('.book-main .page-num').click(function(e) {
     update_hash.apply($(this).closest('.row'));
   });
